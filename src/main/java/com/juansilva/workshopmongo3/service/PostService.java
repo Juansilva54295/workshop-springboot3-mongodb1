128ba2a536fc1b6315405901a73479cee87625ea
Juan Silva <juanportela100@gmail.com>
2025-02-15 02:04:42 -0300

    Buscar post por id

---
+++ src/main/java/com/juansilva/workshopmongo3/service/PostService.java
@@ -0,0 +1,23 @@
+package com.juansilva.workshopmongo3.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juansilva.workshopmongo3.dominio.Post;
+import com.juansilva.workshopmongo3.repository.PostRepository;
+import com.juansilva.workshopmongo3.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	
+	public Post findById(String id) {
+		return repo.findById(id)
+			.orElseThrow(() -> new ObjectNotFoundException("OBJETO NÃO ENCONTRADO"));
+	}
+	
+
+}
