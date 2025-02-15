5005f127ccd350455e126181430fd3562932f7b9
Juan Silva <juanportela100@gmail.com>
2025-02-14 22:12:54 -0300

    Projeto e classe criados

---
+++ src/main/java/com/juansilva/workshopmongo3/Workshopmongo3Application.java
@@ -0,0 +1,13 @@
+package com.juansilva.workshopmongo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshopmongo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshopmongo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/juansilva/workshopmongo3/Workshopmongo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juansilva.workshopmongo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopmongo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
